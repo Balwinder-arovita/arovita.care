0f5f92d2454021e882dd82a89462a489314ae48f
Boby <1231262balwindersingh@gmail.com>
2026-04-16 21:56:31 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from 'vite' // <--- This is the missing line
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   base: '/arovita.care/', 
