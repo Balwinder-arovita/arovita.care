599f35e9706d1dc6153c0967e72cc10085be104c
Boby <1231262balwindersingh@gmail.com>
2026-04-16 22:02:09 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* Add the basename to match your GitHub repository name */}
+    <BrowserRouter basename="/arovita.care">
       <App />
     </BrowserRouter>
   </StrictMode>,
